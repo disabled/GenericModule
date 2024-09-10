87da8c4fc8dae23be5f3f4c30196191c108b9eb0
Anton Kormakov <anton.kormakov@rosberry.com>
2024-09-10 12:09:00 +0600

    Add ViewNib convenience functions

--- Sources/GenericModule/View/ViewNib.swift
+++ Sources/GenericModule/View/ViewNib.swift
@@ -2,14 +2,38 @@
 //  Copyright © 2024 Luma AI. All rights reserved.
 //
 
+import UIKit
 import Foundation
 
 public class ViewNib {
-    let name: String?
-    let bundle: Bundle?
+    public let name: String?
+    public let bundle: Bundle?
 
     public init(name: String?, bundle: Bundle?) {
         self.name = name
         self.bundle = bundle
+    }
+
+    public func instantiate(owner: Any?, options: [UINib.OptionsKey: Any]? = nil) -> [Any] {
+        guard let name = name else {
+            return []
+        }
+
+        let source = UINib(nibName: name, bundle: bundle)
+        return source.instantiate(withOwner: owner, options: options)
+    }
+
+    public func instantiate<T>(owner: Any?, options: [UINib.OptionsKey: Any]? = nil) -> T? {
+        guard let name = name else {
+            return nil
+        }
+
+        let source = UINib(nibName: name, bundle: bundle)
+        let contents = source.instantiate(withOwner: owner, options: options)
+        for case let object as T in contents {
+            return object
+        }
+
+        return nil
     }
 }
